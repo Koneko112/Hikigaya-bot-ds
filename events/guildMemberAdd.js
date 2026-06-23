acd35070b97b1619fbfcb5d7ad30e7b4bdbdfa70
Koneko112 <dimakom337@gmail.com>
2026-06-24 03:20:48 +1000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,25 +1,10 @@
-const configManager = require('../config/configManager');
+const economyManager = require('../config/economyManager');
 
 module.exports = {
     name: 'guildMemberAdd',
     execute(member) {
-        const config = configManager.getConfig();
-        
-        // Приветственное сообщение
-        if (config.welcomeMessage) {
-            const channel = member.guild.systemChannel;
-            if (channel) {
-                const message = config.welcomeMessage.replace('{user}', `<@${member.id}>`);
-                channel.send(message);
-            }
-        }
-        
-        // Автороль
-        if (config.autoRoleId) {
-            const role = member.guild.roles.cache.get(config.autoRoleId);
-            if (role) {
-                member.roles.add(role).catch(console.error);
-            }
-        }
+        // Тут будет логика проверки реферального кода
+        // Мы сделаем её позже, через сессию и веб-интерфейс
+        console.log(`👤 Новый участник: ${member.user.tag}`);
     }
 };
