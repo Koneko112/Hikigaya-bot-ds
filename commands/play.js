2a18bdd6a453ba11c8cf9a4b1c20e05ac09c6869
Koneko112 <dimakom337@gmail.com>
2026-06-29 01:07:42 +1000

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,5 @@
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
+const ytdl = require('@distube/ytdl-core');
 const ytSearch = require('yt-search');
 
 const queue = new Map();
