0f34d94511dfac1aa0c7dd0f04ed53bb0faa988b
Koneko112 <dimakom337@gmail.com>
2026-06-29 01:43:02 +1000

    Update player.js

--- utils/player.js
+++ utils/player.js
@@ -1,47 +1,10 @@
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
+const { Player } = require('discord-player');
+const { Extractor } = require('@discord-player/extractor');
 
-const queue = new Map();
+// Создаём плеер
+const player = new Player();
 
-async function playSong(interaction, serverQueue, voiceChannel) {
-    if (!serverQueue.songs.length) {
-        queue.delete(interaction.guildId);
-        return;
-    }
+// Загружаем экстракторы (YouTube, SoundCloud, Spotify)
+player.extractors.loadDefault();
 
-    const song = serverQueue.songs[0];
-    
-    const connection = joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: interaction.guildId,
-        adapterCreator: interaction.guild.voiceAdapterCreator,
-    });
-
-    const player = createAudioPlayer();
-    
-    const stream = ytdl(song.url, { 
-        filter: 'audioonly',
-        quality: 'lowestaudio',
-        highWaterMark: 1 << 25
-    });
-    
-    const resource = createAudioResource(stream);
-    player.play(resource);
-    connection.subscribe(player);
-
-    serverQueue.player = player;
-    serverQueue.connection = connection;
-
-    player.on(AudioPlayerStatus.Idle, () => {
-        serverQueue.songs.shift();
-        playSong(interaction, serverQueue, voiceChannel);
-    });
-
-    player.on('error', (error) => {
-        console.error('Ошибка плеера:', error);
-        serverQueue.songs.shift();
-        playSong(interaction, serverQueue, voiceChannel);
-    });
-}
-
-module.exports = { playSong, queue };
+module.exports = player;
