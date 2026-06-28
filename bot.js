5b1780f3b9c3202b493bcd5a82c228a2cc79eed7
Koneko112 <dimakom337@gmail.com>
2026-06-29 01:53:47 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,7 @@ const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
 const path = require('path');
 const fs = require('fs');
-const player = require('./utils/player');
 
-// Делаем плеер доступным глобально
-global.player = player;
 // ====== ИНИЦИАЛИЗАЦИЯ БОТА ======
 const client = new Client({
     intents: [
