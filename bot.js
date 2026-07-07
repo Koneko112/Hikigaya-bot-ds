8221e0fdb0f100d6878476d80bc25d6ae1cf954c
Koneko112 <dimakom337@gmail.com>
2026-07-07 13:03:14 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,8 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates 
     ]
 });
 global.discordClient = client;
