a504922cf3e4406e401f83fda2d50322ed297d96
Koneko112 <dimakom337@gmail.com>
2026-07-07 14:19:31 +1000

    Delete utils/player.js

--- utils/player.js
+++
@@ -1,9 +0,0 @@
-const { Player } = require('discord-player');
-const { Extractor } = require('@discord-player/extractor');
-
-const player = new Player();
-
-// Загружаем экстракторы для YouTube и локальных файлов
-player.extractors.loadDefault();
-
-module.exports = player;
