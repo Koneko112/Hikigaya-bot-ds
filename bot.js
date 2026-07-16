d1ba615d7dc6183043da073fe56886f3c451c44a
Koneko112 <dimakom337@gmail.com>
2026-07-16 16:07:39 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,9 +71,9 @@ passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
 
 passport.use(new DiscordStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL,
+    clientID: '1506710063446757477',
+    clientSecret: 'sCbw-6Q_Drg3BhcdZuiYV4yQ9ugElO3h',
+    callbackURL: 'https://hikigaya-bot-ds.onrender.com/auth/discord/callback',
     scope: ['identify', 'guilds']
 }, (accessToken, refreshToken, profile, done) => {
     process.nextTick(() => done(null, profile));
